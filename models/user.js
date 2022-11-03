c4acfb7c14ce9cfd944174dbbf1d1cd2e0b27673
usama-pixel <usamaali052@gmail.com>
2022-11-03 18:39:34 +0500

    application's all crud opperations revamped to use mongodb

--- models/user.js
+++ models/user.js
@@ -69,8 +69,18 @@ class User {
 
   addOrder() {
     const db = getDb()
-    return db.collection('orders')
-      .insertOne(this.cart)
+    return this.getCart()
+      .then(products => {
+        const order = {
+          items: products,
+          user: {
+            _id: new ObjectId(this._id),
+            name: this.name,
+          }
+        }
+        return db.collection('orders')
+          .insertOne(order)
+      })
       .then(result => {
         this.cart = { items: [] }
         return db.collection('users')
@@ -79,6 +89,14 @@ class User {
             { $set: { cart: { items: [] } } }
           )
       })
+  }
+
+  getOrders() {
+    const db = getDb()
+    return db
+      .collection('orders')
+      .find({ 'user._id': new ObjectId(this._id) })
+      .toArray()
   }
 
   static findById(userId) {
