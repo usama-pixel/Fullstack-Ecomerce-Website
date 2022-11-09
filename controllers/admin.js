71f590076059eb28da5798ced3abcc24c9e7435f
usama-pixel <usamaali052@gmail.com>
2022-11-09 16:55:33 +0500

    Validation of product editing page inputs done

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,4 +1,5 @@
 const { ObjectId } = require('mongodb')
+const { validationResult } = require('express-validator')
 const Product = require('../models/product')
 
 exports.getAddProduct = (req, res, next) => {
@@ -6,10 +7,25 @@ exports.getAddProduct = (req, res, next) => {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
     editing: false,
+    hasError: false,
+    validationErrors: [],
+    errorMessage: null
   })
 }
 
 exports.postAddProduct = (req, res, next) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(422).render('admin/edit-product', {
+      pageTitle: 'Add Product',
+      path: '/admin/add-product',
+      editing: false,
+      hasError: true,
+      validationErrors: errors.array(),
+      errorMessage: errors.array()[0].msg,
+      product: { ...req.body }
+    })
+  }
   const product = new Product({
     ...req.body,
     userId: req.user._id
@@ -17,7 +33,6 @@ exports.postAddProduct = (req, res, next) => {
   // to simply req.user which is a mongoose object, and mongoose will automatically pick the _id from it.
   product.save()
     .then(result => {
-      console.log('result', result)
       res.redirect('/admin/products')
     })
     .catch(err => console.log(err))
@@ -37,6 +52,9 @@ exports.getEditProduct = (req, res, next) => {
         pageTitle: 'Edit Product',
         path: '/admin/edit-product',
         editing: editMode,
+        hasError: false,
+        errorMessage: '',
+        validationErrors: [],
         product,
       })
     })
@@ -44,8 +62,25 @@ exports.getEditProduct = (req, res, next) => {
 }
 
 exports.postEditProduct = (req, res, next) => {
+  const errors = validationResult(req)
+  const prodId = req.body.productId
+  console.log('submit', req.body)
+  // console.log(errors.array())
+  if (!errors.isEmpty()) {
+    return res.status(422).render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: true,
+      hasError: true,
+      validationErrors: errors.array(),
+      errorMessage: errors.array()[0].msg,
+      product: { ...req.body, _id: prodId }
+    })
+  }
+  console.log('outside')
   Product.findById(req.body.productId)
     .then(product => {
+      console.log('mongo product', product)
       if (product.userId.toString() !== req.user._id.toString())
         return res.redirect('/')
 
--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { body } = require('express-validator')
 const path = require('path')
 
 const adminController = require('../controllers/admin')
@@ -13,11 +14,45 @@ router.get('/add-product', isAuth, adminController.getAddProduct)
 router.get('/products', isAuth, adminController.getProducts)
 
 // /admin/add-product => post
-router.post('/add-product', isAuth, adminController.postAddProduct)
+router.post(
+  '/add-product',
+  isAuth,
+  [
+    body('title', 'Title must contain characters and numbers')
+      .isString()
+      .isLength({ min: 3 })
+      .trim(),
+    body('imageUrl', 'Image URL must be provided').isURL(),
+    body('price')
+      .isFloat({ min: 0.0001 })
+      .withMessage('Your price must be greater than 0'),
+    body('description')
+      .isLength({ min: 5, max: 400 })
+      .trim()
+  ],
+  adminController.postAddProduct
+)
 
 router.get('/edit-product/:productId', isAuth, adminController.getEditProduct)
 
-router.post('/edit-product', isAuth, adminController.postEditProduct)
+router.post(
+  '/edit-product',
+  isAuth,
+  [
+    body('title', 'Title must contain characters and numbers')
+      .isString()
+      .isLength({ min: 3 })
+      .trim(),
+    body('imageUrl', 'Image URL must be provided').isURL(),
+    body('price')
+      .isFloat({ min: 0.0001 })
+      .withMessage('Your price must be greater than 0'),
+    body('description',)
+      .isLength({ min: 5, max: 400 })
+      .withMessage('A description of at least 5 characters must be provided')
+      .trim()
+  ],
+  adminController.postEditProduct)
 
 router.post('/delete-product', isAuth, adminController.deleteProduct)
 
