d16052ab2ca44ee14ac77156b5c18a53407280bf
usama-pixel <usamaali052@gmail.com>
2022-11-10 18:46:06 +0500

    added pagination to products page

--- controllers/shop.js
+++ controllers/shop.js
@@ -6,16 +6,31 @@ const Product = require('../models/product')
 const Order = require('../models/order')
 const User = require('../models/user')
 
-const ITEMS_PER_PAGE = 1
+const ITEMS_PER_PAGE = 2
 
 exports.getProducts = (req, res, next) => {
-  Product.find()
+  const page = +req.query.page || 1
+  let totalItems;
+  Product
+    .find()
+    .countDocuments()
+    .then(numProducts => {
+      totalItems = numProducts
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE)
+    })
     .then(products => {
-      console.log(products)
       res.render('shop/product-list', {
         prods: products,
-        pageTitle: 'All Products',
+        pageTitle: 'Products',
         path: '/products',
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
       })
     })
     .catch(err => {
